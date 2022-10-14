5275d9735679e4ea2c0214210235caae16908008
Zeeroon-e <chia.kmohamad@hotmail.com>
2022-10-14 13:45:58 +0200

    overlay css and tsx code

--- src/components/AdminOrderItem.tsx
+++ src/components/AdminOrderItem.tsx
@@ -1,5 +1,12 @@
 import './AdminOrderItem.scss';
+import AdminEditOverlay from './overlays/AdminEditOrder'
+import { useState } from 'react'
+
 function AdminOrderItem() {
+    const [openEdit, setOpenEdit] = useState(false);
+    const editPressed = () => {
+        {setOpenEdit(true)}
+    }
     return (
         <div className="admin-order-item">
             <div className='admin-order-card'>
@@ -21,9 +28,10 @@ function AdminOrderItem() {
                 <h4 className='admin-card-total-price'>Totalt: 180:-</h4>
                 <input className='admin-card-input' type="text" />
                 <div className='card-buttons'>
-                    <button className='edit-btn'>Edit</button>
+                    <button className='edit-btn' onClick={editPressed}>Edit</button>
                     <button className='status-btn'>Preparing</button>
                 </div>
+                {openEdit && <AdminEditOverlay closeOverlay={setOpenEdit} />}
             </div>
         </div>
     )
--- src/components/overlays/AdminEditOrder.tsx
+++ src/components/overlays/AdminEditOrder.tsx
@@ -1,7 +1,35 @@
-function AdminEditOrder() {
+import './AdminEditOrder.scss'
+import { useNavigate } from 'react-router-dom'
+
+interface Prop {
+    closeOverlay: (close: boolean) => void;
+
+}
+
+function AdminEditOrder( {closeOverlay}: Prop) {
+    const navigate = useNavigate();
+    const closeBtn = () => {
+        closeOverlay(false)
+        navigate('/admin')
+    }
     return (
-        <>
-        </>
+        <section className="admin-edit-overlay-container">
+            <div className='admin-edit-overlay'>
+                <h1>Edit Order</h1>
+                <div className='card-dish'>
+                    <p>Dish 1</p>
+                    <p>90:-</p>
+                    <button className='delete-dish-btn'>Delete</button>
+                </div>
+                <h2>Total: 180:-</h2>
+                <input type="text" />
+                <input type="text" />
+                <button>Change Order</button>
+                <button onClick={closeBtn}>Close</button>
+            </div>
+            
+
+        </section>
     )
 }
 
