2352d4fb55da84dbecb22ffa3bb913a6f0a972f2
ammarfolk <ammar.mawaldi@gmail.com>
2022-10-14 13:51:15 +0200

    transferred all to a UserOrderItem component, and imported to UserOrder

--- src/Views/User/UserOrders.tsx
+++ src/Views/User/UserOrders.tsx
@@ -1,4 +1,5 @@
 import React from "react";
+import OrderItem from "../../components/UserOrderItem";
 import "./UserOrders.scss";
 
 type Props = {};
@@ -9,39 +10,9 @@ const UserOrders = (props: Props) => {
       <img src="../src/assets/user-orders-title.svg" alt="My Order" />
 
       <section className="wrapper">
-        <section className="card">
-          <section className="card__header">
-            <section className="card__status">
-              <div className="status__color"></div>
-              <p className="status__text">Status</p>
-            </section>
-            <p className="order__number">Ordernr 1001</p>
-          </section>
-          <p className="card__date">2022-10-07 kl 16:38</p>
-
-          <section className="card__info">
-            <section className="card__order">
-              <p className="card__text">Ratatouille</p>
-              <p className="card__text">90:-</p>
-            </section>
-
-            <section className="card__order">
-              <p className="card__text">Ratatouille Veg.</p>
-              <p className="card__text">75:-</p>
-            </section>
-
-            <section className="card__order">
-              <p className="card__text">Coca Cola</p>
-              <p className="card__text">25:-</p>
-            </section>
-          </section>
-
-          <p className="card__cost">Totalt: 190:-</p>
-          <section className="card__btns">
-            <button className="card__btn__edit">Edit</button>
-            <button className="card__btn__delete">Delete</button>
-          </section>
-        </section>
+        <OrderItem />
+        <OrderItem />
+        <OrderItem />
       </section>
     </section>
   );
--- src/components/UserOrderItem.tsx
+++ src/components/UserOrderItem.tsx
@@ -1,11 +1,42 @@
-import React from 'react'
+import React from "react";
+import "./UserOrderItem.scss";
 
 function UserOrderItem() {
   return (
-    <div>
-      
-    </div>
-  )
+    <section className="card">
+      <section className="card__header">
+        <section className="card__status">
+          <div className="status__color"></div>
+          <p className="status__text">Status</p>
+        </section>
+        <p className="order__number">Ordernr 1001</p>
+      </section>
+      <p className="card__date">2022-10-07 kl 16:38</p>
+
+      <section className="card__info">
+        <section className="card__order">
+          <p className="card__text">Ratatouille</p>
+          <p className="card__text">90:-</p>
+        </section>
+
+        <section className="card__order">
+          <p className="card__text">Ratatouille Veg.</p>
+          <p className="card__text">75:-</p>
+        </section>
+
+        <section className="card__order">
+          <p className="card__text">Coca Cola</p>
+          <p className="card__text">25:-</p>
+        </section>
+      </section>
+
+      <p className="card__cost">Totalt: 190:-</p>
+      <section className="card__btns">
+        <button className="card__btn__edit">Edit</button>
+        <button className="card__btn__delete">Delete</button>
+      </section>
+    </section>
+  );
 }
 
-export default UserOrderItem
+export default UserOrderItem;
