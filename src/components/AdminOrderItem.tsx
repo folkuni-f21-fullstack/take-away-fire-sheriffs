2d61b5d351b0419040b2902fc7de6e15ebfab2fa
Zeeroon-e <chia.kmohamad@hotmail.com>
2022-10-14 11:02:33 +0200

    html css done for adminview
    next step is to do the edit overlay

--- src/components/AdminOrderItem.tsx
+++ src/components/AdminOrderItem.tsx
@@ -1,7 +1,31 @@
+import './AdminOrderItem.scss';
 function AdminOrderItem() {
     return (
-        <>
-        </>
+        <div className="admin-order-item">
+            <div className='admin-order-card'>
+                <div className='card-upper-section'>
+                    <div className='status'></div>
+                    <h4 className='status-text'>Preparing</h4>
+                    <h4 className='orderNr'>orderNr 1001</h4>
+                </div>
+                <p className='order-date'> 2022-10-07 kl 16:38</p>
+                <div className='card-dish'>
+                    <p>Dish 1</p>
+                    <p>90:-</p>
+                </div>
+                <div className='card-dish'>
+                    <p>Dish 1</p>
+                    <p>90:-</p>
+                </div>
+                
+                <h4 className='admin-card-total-price'>Totalt: 180:-</h4>
+                <input className='admin-card-input' type="text" />
+                <div className='card-buttons'>
+                    <button className='edit-btn'>Edit</button>
+                    <button className='status-btn'>Preparing</button>
+                </div>
+            </div>
+        </div>
     )
 }
 
