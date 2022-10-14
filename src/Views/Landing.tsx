0bd4470a445fa54e7f6caf8451a0d9c86338a2eb
Monica Björk <m.bjork95@gmail.com>
2022-10-14 11:18:17 +0200

    added components and some styling

--- src/Views/Landing.tsx
+++ src/Views/Landing.tsx
@@ -1,7 +1,20 @@
+import Header from '../components/Header';
+import logo from '../assets/logo.svg';
+import './Landing.scss';
+
 function Landing() {
     return (
-        <>
-        </>
+        <section className="landingPage">
+            <Header />
+            <img src={logo} />
+            <button className="buttonGuest">Continue as Guest</button>
+            <section className="loginSignup">
+                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="Password" />
+                <button>Log in</button>
+            </section>
+            <button className="buttonMember">Become a Memeber</button>
+        </section>
     )
 }
 
