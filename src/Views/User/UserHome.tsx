1bc0968ef1eb323649353b25b37d372e0b280769
Hampus83 <jhmacgyver@gmail.com>
2022-10-14 11:17:47 +0200

    added menuItem to UserHome + scss

--- src/Views/User/UserHome.tsx
+++ src/Views/User/UserHome.tsx
@@ -1,7 +1,17 @@
+import './UserHome.scss';
+
+import MenuTitle from '../../assets/menu-title.svg';
+
+import MenuItem from '../../components/MenuItem';
+
 function UserHome() {
     return (
-        <>
-        </>
+        <div className='userHomeWrapper'>
+            <img className='menuTitle' src={ MenuTitle } alt="" />
+            <MenuItem />
+            <MenuItem />
+            <MenuItem />
+        </div>
     )
 }
 
--- src/components/MenuItem.tsx
+++ src/components/MenuItem.tsx
@@ -1,7 +1,20 @@
+import './MenuItem.scss';
+
 function MenuItem() {
     return (
-        <>
-        </>
+        <div className="menuItemWrapper">
+            <section className="menuItem-title-container">
+                <h2>Ratatouille</h2>
+                <h2>90:-</h2>
+            </section>
+            <section className="menuItem-ingredients-container">
+                <p>aubergine, zucchini, tomatoe, red peppers, garlic, beef</p>
+            </section>
+            <section className="menuItem-buttons-container">
+                <button className='menuItem-btn-info'>More info</button>
+                <button className='menuItem-btn-add'>Add to cart</button>
+            </section>
+        </div>
     )
 }
 
