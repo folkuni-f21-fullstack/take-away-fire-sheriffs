887cee2c220c2b60dccb9dcba51dae7231f1a1aa
Zeeroon-e <chia.kmohamad@hotmail.com>
2022-10-16 01:52:50 +0200

    header css and html done, only cart overlay left

--- src/Views/Admin/AdminHome.tsx
+++ src/Views/Admin/AdminHome.tsx
@@ -1,8 +1,10 @@
 import AdminOrderItem from "../../components/AdminOrderItem";
 import './AdminHome.scss';
+import Header from '../../components/Header'
 function AdminHome() {
     return (
         <div className="admin-view">
+            <Header />
             <h1 className="admin-view-title">Orders</h1>
             <section className="admin-view-orders">
                 
