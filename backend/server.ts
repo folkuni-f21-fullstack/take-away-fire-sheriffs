7de14e706727e0ccf1d59bbc5a49c8ad112ca660
Christopher Wahlström <83230387+christopherwahlstrom@users.noreply.github.com>
2022-10-17 15:45:33 +0200

    Worked with requests backend all in team

--- backend/server.ts
+++ backend/server.ts
@@ -1,107 +1,25 @@
 import express, { Request, Response } from 'express'
 const app = express()
-const PORT = 1337  // TODO enviroment variables istället ?? 
 
-
-import { data } from 'data.json'
+const PORT = 1337  // TODO: environment variables istället
 
 import { join, dirname } from 'path'
 import { fileURLToPath } from 'url'
-const __dirname = dirname(fileURLToPath(import.meta.url));
+const __dirname = dirname(fileURLToPath(import.meta.url));  // __dirname i äldre versioner av node
 const staticPath = join( __dirname, '../../dist' )
 
 
-
+// Middleware
 app.use( express.json() )
 
+// app.use( (req, res, next) => {
+//     console.log(`${req.method}  ${req.url} `, req.body)
+//     next()
+// } )
 
-app.use ( (req, res ,next) => {
-    console.log(`${req.method}  ${req.url} `, req.body)
-	next()
-})
-
+// Obs! express.static bör ligga först, när man får många statiska filer
 app.use( express.static(staticPath) )
 
-
-
-// Routes / endpoints
-// Exempel: frontend skickar "GET /api/data", backend tar emot och servar ett svar
-// HTTP methods: GET, POST, PUT, DELETE -> motsvarar CRUD = Create, Read, Update, Delete
-// Dessa fyra används när vi bygger ett RESTful API
-// GET    - hämta data
-// POST   - lägga till ny data
-// PUT    - uppdatera data
-// DELETE - ta bort data
-
-// GET 
-
-
-// Landing page
-app.get('/', (req, res) => {
-    res.send()
-})
-
-
-// About page
-app.get('/about', (req, res) => {
-    res.send()
-})
-
-// Menu page
-
-app.get('/menu', (req, res) => {
-    res.send()
-})
-
-// Orders page
-
-app.get('/orders', (req, res) => {
-    res.send()
-})
-
-// Admin page
-
-app.get('/admin', (req, res) => {
-    res.send()
-})
-
-
-
-// POST  
-
-// Add new order  
-
-app.post('/api/.....', (req, res) => {
-     let newOrder = ??? = req.body
-})
-
-// Add new customer
-
-app.post('/api/...', (req, res) => {
-    let newCustomer = ???  = req.body
-})
-
-// PUT
-
-// Update order to kitchen
-
-app.put('/api/', (req,res) => {
-
-})
-
-
-// DELETE 
-
-// Delete order or product
-
-app.delete('/api/:id', (req, res) => {
-
-})
-
-
-
 app.listen(PORT, () => {
-    console.log(`Server is listening on port ${PORT}.`);
-    
+    console.log(`Server is listening on port ${PORT}.`)
 })
-
