8cb9c8f2515c7fa8fde8a69ad2b318412d4fe724
Monica Björk <m.bjork95@gmail.com>
2022-10-17 09:43:55 +0200

    continue as guest button takes user to the menu

--- src/Views/Landing.tsx
+++ src/Views/Landing.tsx
@@ -1,26 +1,32 @@
+import './Landing.scss';
 import Header from '../components/Header';
 import Signup from '../components/overlays/Signup';
 import logo from '../assets/logo.svg';
-import './Landing.scss';
 import { useState } from 'react';
+import { useNavigate } from 'react-router-dom'
 
 function Landing() {
-    const [openEdit, setOpenEdit] = useState(false);
-    const editPressed = () => {
-        {setOpenEdit(true)}
+    const navigate = useNavigate();
+    const [openSignin, setOpenSignin] = useState(false);
+    const signinClick = () => {
+        {setOpenSignin(true)}
     }
+    function guestUser() {
+        navigate('/menu');
+    }
+
     return (
         <section className="landingPage">
             <Header />
             <img className="logo" src={logo} />
-            <button className="buttonGuest">Continue as Guest</button>
+            <button className="buttonGuest" onClick={guestUser}>Continue as Guest</button>
             <section className="loginSignup">
                 <input type="text" placeholder="Username" />
                 <input type="text" placeholder="Password" />
                 <button>Log in</button>
             </section>
-            <button className="buttonMember" onClick={editPressed}>Become a Member</button>
-            {openEdit && <Signup closeOverlay={setOpenEdit} />}
+            <button className="buttonMember" onClick={signinClick}>Become a Member</button>
+            {openSignin && <Signup closeOverlay={setOpenSignin} />}
         </section>
     )
 }
