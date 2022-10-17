5cb4dbaef7306c775cc9a93065b4a0a173605cf1
Monica Björk <m.bjork95@gmail.com>
2022-10-17 09:16:32 +0200

    finished overlay to become a member

--- src/Views/Landing.tsx
+++ src/Views/Landing.tsx
@@ -2,20 +2,25 @@ import Header from '../components/Header';
 import Signup from '../components/overlays/Signup';
 import logo from '../assets/logo.svg';
 import './Landing.scss';
+import { useState } from 'react';
 
 function Landing() {
+    const [openEdit, setOpenEdit] = useState(false);
+    const editPressed = () => {
+        {setOpenEdit(true)}
+    }
     return (
         <section className="landingPage">
             <Header />
-            <img src={logo} />
+            <img className="logo" src={logo} />
             <button className="buttonGuest">Continue as Guest</button>
             <section className="loginSignup">
                 <input type="text" placeholder="Username" />
                 <input type="text" placeholder="Password" />
                 <button>Log in</button>
             </section>
-            <button className="buttonMember">Become a Memeber</button>
-            <Signup />
+            <button className="buttonMember" onClick={editPressed}>Become a Member</button>
+            {openEdit && <Signup closeOverlay={setOpenEdit} />}
         </section>
     )
 }
--- src/components/overlays/Signup.tsx
+++ src/components/overlays/Signup.tsx
@@ -1,12 +1,26 @@
 import './Signup.scss';
+import close from '../../assets/close-overlay-button.svg';
 
-function Signup() {
+interface Prop {
+    closeOverlay: (close: boolean) => void;
+
+}
+
+function Signup( {closeOverlay}: Prop ) {
+    const closeBtn = () => {
+        closeOverlay(false);
+    }
+
     return (
-        <section className="overlay loginSignup">
-            <input type="text" placeholder="Username" />
-            <input type="text" placeholder="Password" />
-            <button>Log in</button>
-            <p className="signupInfo">Att ha nog av nog är alltid nog. </p>
+        <section className="overlay">
+            <section className="loginSignup">
+                <img src={close} onClick={closeBtn} alt="close" className="closeButton" />
+
+                <input type="text" placeholder="Username" />
+                <input type="text" placeholder="Password" />
+                <button>Log in</button>
+                <p className="signupInfo">Att ha nog av nog är alltid nog. </p>
+            </section>
         </section>
     )
 }
