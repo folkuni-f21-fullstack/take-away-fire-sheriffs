82d8d8da58ab058a94a567512f8c6cee223796f9
Monica Björk <m.bjork95@gmail.com>
2022-10-25 16:05:54 +0200

    shows feedback message to user

--- src/components/overlays/Signup.tsx
+++ src/components/overlays/Signup.tsx
@@ -1,13 +1,15 @@
 import './Signup.scss';
 import close from '../../assets/close-overlay-button.svg';
 import { Credentials, User } from '../../models/models';
-import { useState } from 'react';
+import { useEffect, useState } from 'react';
+import { response } from 'express';
 
 interface Prop {
     closeOverlay: (close: boolean) => void;
 }
 
 function Signup( {closeOverlay}: Prop ) {
+    const [feedback, setFeedback] = useState('');
     const [username, setUsername] = useState('');
     const [password, setPassword] = useState('');
     const minUsernameCharacters: number = 3;
@@ -17,28 +19,38 @@ function Signup( {closeOverlay}: Prop ) {
     }
 
     async function userSignup() {
-        if(username.length >= minUsernameCharacters && password.length >= minPasswordCharacters) {
-            const userExsists: boolean = await doesUserAlreadyExist();
-            console.log("userExsists: ",userExsists);
-            if(!userExsists){
-                const credentials: Credentials = {
-                    username: username,
-                    password: password
-                }
-        
-                const requestOptions = {
-                    method: 'POST',
-                    headers: { 'Content-Type': 'application/json' },
-                    body: JSON.stringify(credentials)
-                }
+        if(username.length >= minUsernameCharacters) {
+            if(password.length >= minPasswordCharacters) {
+                const userExsists: boolean = await doesUserAlreadyExist();
+                console.log("userExsists: ",userExsists);
+                if(!userExsists){
+                    const credentials: Credentials = {
+                        username: username,
+                        password: password
+                    }
+            
+                    const requestOptions = {
+                        method: 'POST',
+                        headers: { 'Content-Type': 'application/json' },
+                        body: JSON.stringify(credentials)
+                    }
 
-                const response = await fetch('api/users/signup', requestOptions);
+                    const response = await fetch('api/users/signup', requestOptions);
+                    checkResponse(response);
+                } else {
+                    setFeedback(`The username "${username}" already exists, please choose another one`);
+                }
+            } else {
+                setFeedback(`Your password needs to be longer than ${minPasswordCharacters} characters`);
             }
+        } else {
+            setFeedback(`Your username needs to be longer than ${minUsernameCharacters} characters`);
         }
+    }
 
+    function checkResponse(response: Response){
 
-
-        // Kontrollera svaret 
+        console.log(response);
     }
 
     async function doesUserAlreadyExist() {
@@ -61,7 +73,7 @@ function Signup( {closeOverlay}: Prop ) {
                 <input type="text" placeholder="Username" value={username} onChange={(e) => setUsername(e.target.value)} />
                 <input type="text" placeholder="Password" value={password} onChange={(e) => setPassword(e.target.value)} />
                 <button onClick={userSignup} >Sign up</button>
-                <p className="signupInfo"> </p>
+                <p className="signupInfo">{feedback}</p>
             </section>
         </section>
     )
