48a401ceec905d8b452d44b349b8c9fcbf0b66e0
Hampus83 <jhmacgyver@gmail.com>
2022-10-26 16:09:33 +0200

    all functionality in place

--- backend/routes/orders.ts
+++ backend/routes/orders.ts
@@ -3,6 +3,12 @@ import db from "../db.js";
 import { Orders, Users, Menu } from "../models";
 const router = express.Router();
 
+type Query = {
+    username: string;
+    order: Orders;
+    comment: string;
+  }
+
 router.get('/', (req, res) => {
     if (db.data) {
         console.log(db.data);
@@ -39,9 +45,15 @@ router.delete('/deleteorder', async (req, res) => {
                     res.json(newUserOrders);
                     db.write();
                     return;
+                } else {
+                    // statuscode
                 }
             });
+        } else {
+            // statuscode
         }
+    } else {
+        // statuscode
     } 
 });
 
@@ -65,24 +77,41 @@ router.delete('/deleteitem', async (req, res) => {
             if (itemsAfter) {
                 res.send(itemsAfter);
                 db.write();
-
             } else {
                 res.sendStatus(404);
             }  
+        } else {
+            // statuscode
         } 
     });
     
 });
 
-router.post('/usercomment', (res, req) => {
+router.post('/usercomment', (req, res) => {
     if (!db.data) {
         res.sendStatus(404);
         return;
     }
 
-    const query = req.body;
-
+    const query: Query = req.body;
+    
     const user = db.data.users.find(user => user.username === query.username);
+
+    if (user) {
+        user.orders.map(order => {
+            if (order.id === query.order.id) {
+                order.userComment = query.comment;
+                console.log(order.userComment);
+                res.json(order.userComment);
+                db.write();  
+            } else {
+                // statuscode
+            }
+        })
+    } else {
+        // statuscode
+    }
+    
 });
 
 export default router;
